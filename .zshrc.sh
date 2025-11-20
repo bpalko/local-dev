296e2cff1a28417247ed0b0a30dc06f416d41363
Brendan Palkowski <b.palko@outlook.com>
2025-11-19 19:31:22 -0500

    chore: some updates to the zshrc file

--- .zshrc.sh
+++ .zshrc.sh
@@ -13,6 +13,7 @@ export ZSH="$HOME/.oh-my-zsh"
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
 # ZSH_THEME="robbyrussell"
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 plugins=(
 git 
@@ -29,14 +30,9 @@ export PATH="${PATH}:${HOME}/.krew/bin"
 # Source oh my zsh
 source $ZSH/oh-my-zsh.sh
 
-# Power10k
-ZSH_THEME="powerlevel10k/powerlevel10k"
-source /opt/homebrew/opt/powerlevel10k/powerlevel10k.zsh-theme
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
-# This is a fun one
-eval $(thefuck --alias)
 
 # Golang
 export GOPATH=$HOME/go
